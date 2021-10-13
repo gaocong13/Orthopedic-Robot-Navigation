fa8e70e65f909a7e69807218f48031716bc1dae2
Cong Gao <cgao11@jhu.edu>
2021-10-13 14:45:37 -0400

    temporarily remove Spine folder

--- apps/Spine/cmd/cmd_device_singleview.sh
+++
@@ -1,7 +0,0 @@
-LandmarkAnnot=/home/cong/Research/Spine/CadaverNeedleInjection/Registration/Mar16/landmark_annotation
-Device3DLandmark=/home/cong/Research/Spine/CadaverNeedleInjection/meta_data/Device3Dlandmark.fcsv
-Device3DBB=/home/cong/Research/Spine/CadaverNeedleInjection/meta_data/Device3Dbb.fcsv
-ExpTXT=/home/cong/Research/Spine/CadaverNeedleInjection/expID.txt
-ImageDCM=/home/cong/Research/Spine/CadaverNeedleInjection/Registration/Mar16/RealXraydcm
-Output=/home/cong/Research/Spine/CadaverNeedleInjection/output_singleview
-xreg-spine-device-regi-singleview ${LandmarkAnnot} ${Device3DLandmark} ${Device3DBB} ${ExpTXT} ${ImageDCM} ${Output} -v
--- apps/Spine/cmd/cmd_spine_singleview.sh
+++
@@ -1,8 +0,0 @@
-CurPath=/home/cong/Research/Spine/CadaverNeedleInjection
-RootPath=/home/cong/Research/Spine/CadaverNeedleInjection/Registration/Mar16
-LandmarkAnnot=${RootPath}/landmark_annotation
-MetaData=${CurPath}/meta_data
-ExpTXT=${CurPath}/expID.txt
-ImageDCM=${RootPath}/RealXraydcm
-Output=${CurPath}/output_spine_singleview
-xreg-spine-spine-regi-singleview ${LandmarkAnnot} ${MetaData} ${ExpTXT} ${ImageDCM} ${Output} -v
